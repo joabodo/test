d4710d7e13e032984a6f2a08a30b6d93bd9b3653
LancemDev <munyaolance1@gmail.com>
2024-09-20 05:51:32 +0300

    :bug: Bug: Relationaship between sacco_admin and sacco

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.2.7/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
     <div>
